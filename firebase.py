a16e6947ff47ac0441967f509a9d15a4e2cb7178
John Doe <johndoe@example.com>
2020-02-23 03:01:39 +0900

    update

--- firebase.py
+++ firebase.py
@@ -3,11 +3,16 @@ import getopt
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
+import os
 import re
 import sys
 
 # export GOOGLE_APPLICATION_CREDENTIALS='/c/Users/kazuh/AppData/Roaming/gcloud/directed-portal-xxxxxx.json'
 def main():
+
+    with open(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')) as f:
+        df = json.load(f)
     try:
         opts, args = getopt.getopt(sys.argv[1:], "ho:cdev", ["help", "env=", "output="])
     except getopt.GetoptError as err:
@@ -19,7 +24,7 @@ def main():
     output = None
     verbose = False
 
-    mc = MyClass(opts,args)
+    mc = MyClass(df['project_id'], opts,args)
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
@@ -33,7 +38,6 @@ def main():
             output = a
         else:
             assert False, "unhandled option"
-        print('-------------{}'.format(verbose))
     # ...
     # print(opts)
     # print(args)
@@ -42,24 +46,23 @@ def usage():
     print("usage")
 
 class MyClass:
-    project_id = 'directed-portal-268501'
     enviroment = 'dev'
-    def __init__(self, opts, args):
+    def __init__(self, project_id, opts, args):
         print('[{}] START'.format(datetime.datetime.now()))
         cred = credentials.ApplicationDefault()
         firebase_admin.initialize_app(cred, {
-            'projectId': self.project_id,
+            'projectId': project_id,
         })
         for a in args:
             m = re.search('env=(prd|stg|dev)', a)
             if m.group(1) is not None:
                 self.enviroment = m.group(1)
-                print(self.enviroment)
         self.db = firestore.client()
+        print(u'[enviroment]: {}'.format(self.enviroment))
         self.collection = self.db.collection(u'{}-jnl'.format(self.enviroment)).document(u'status').collection(u'jnl')
 
     def count_doc(self):
-        doc_ref = self.db.collection(u'dev-jnl').document(u'status').collection(u'jnl') \
+        doc_ref = self.db.collection(u'{}-jnl'.format(self.enviroment)).document(u'status').collection(u'jnl') \
             .order_by(u'fid', direction=firestore.Query.DESCENDING).stream()
         # print(doc_ref)
         cnt = 0
